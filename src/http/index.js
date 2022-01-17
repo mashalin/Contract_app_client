101a4f61cd8f969c7129e94d4e87908a1cc138e6
mashalin <mashatrushel666@gmail.com>
2022-01-17 09:42:22 +0300

    add api

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_HOST
+    //baseURL: process.env.REACT_APP_HOST
+    baseURL: 'http://pkd.bsmu.by'
 })
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_HOST
+    //baseURL: process.env.REACT_APP_HOST
+    baseURL: 'http://pkd.bsmu.by'
 })
 
 const authInterceptor = config => {
